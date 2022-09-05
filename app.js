f0b54453e494376f40fdef9d7d91f5ea4fd040c6
Nadeem Khan <profuse.nk786@gmail.com>
2022-09-05 20:49:26 +0530

    server side for login and registration page

---
+++ app.js
@@ -0,0 +1,81 @@
+const express=require("express");
+const ejs=require("ejs");
+const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
+
+const app=express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine','ejs');
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+const userSchema={
+    name:String,
+    mobNumber: Number,
+    intrestedIn:String,
+    expectetedRange:String,
+    email:String,
+    password : String
+    
+}
+
+const User=mongoose.model("User",userSchema);
+
+app.get("/",function(req,res){
+    res.send("<h1>Welcome To User Login Page</h1>");
+});
+
+app.post("/register",function(req,res){
+
+    console.log(req.body);
+    
+    const newUser=new User({
+
+    name:req.body.name,
+    mobNumber: req.body.mobNumber,
+    intrestedIn:req.body.phone,
+    ExpectetedRange:req.body.range,
+    email:req.body.email,
+    password : req.body.password
+});
+
+newUser.save(function (err) {
+    if(!err){
+        res.send(`Thanks For Registration Mr/Mrs. ${req.body.name}`);
+    }else{
+        console.log(err);
+    }
+})
+
+})
+
+app.post("/login",function(req,res){
+
+    console.log(req.body);
+   
+    User.findOne({mobNumber:req.body.userName},function(err,foundUser){
+        if(!err){
+            if(foundUser.password===req.body.password){
+                
+                res.send(`You Are Successfully LogedIn Mr/Mrs. ${foundUser.name}`);
+            }else{
+                res.send("User id And Password Dosen't Matched ");
+            }
+        }else{
+            console.log(err);
+        }
+    })
+})
+
+
+
+
+
+
+
+
+app.listen(3000,function(req,res){
+    console.log("Server is running on localhost 3000");
+});
