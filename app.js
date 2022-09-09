507f5f5f30a90dff5d9f5d1179d6da6f80750b01
Nadeem Khan <profuse.nk786@gmail.com>
2022-09-09 20:40:27 +0530

    added final login and register page

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 const userSchema={
     name:String,
     mobNumber: Number,
-    intrestedIn:String,
     expectetedRange:String,
+    intrestedIn:String,
     email:String,
     password : String
     
