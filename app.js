f8b99c29d17dffe60705885d934009546ea6da22
Nadeem Khan <profuse.nk786@gmail.com>
2022-09-10 21:32:27 +0530

    Owner Login And Registration pages  Added and Mapped With Db and temperory  Navbar added for moving to each page

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine','ejs');
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+mongoose.connect("mongodb://127.0.0.1:27017/mobileShopDB");
+
+// **********Schema For Users Registration**************//
 
 const userSchema={
     name:String,
@@ -21,17 +23,79 @@ const userSchema={
     
 }
 
+
+// **********Schema For Owner Registration**************//
+
+const ownerSchema={
+    name:String,
+    email:String,
+    password:String
+}
+
+// **********Schema For Distrbuter Invoice or Stocklist**************//
+
+//Invoice Number
+//Distributer Name
+//Gst. Number og dist.
+//item Number
+//Brand Name
+//Model Name
+//Varrinet
+//Color
+//price/unit
+//Quantity
+//total Amount
+//Net Amount
+
+//Payment Method
+//Amount Paid
+//Amount Balance
+
+
+
+
+// **********Model And Collection  For Users**************//
+
 const User=mongoose.model("User",userSchema);
+
+// **********Model And Collection  For Owner**************//
+
+const Owner=mongoose.model("Owner",ownerSchema);
+
+
+
+
+// **********Get Request For Home Page /Owner Login**************//
 
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/login.html");
 });
 
+// **********Get Request For Customer Registration**************//
+
 app.get("/register",function(rec,res){
 
     res.sendFile(__dirname+"/register.html");
 
-})
+});
+
+// **********Get Request For Owner Login**************//
+
+app.get("/ownerlogin",function(req,res){
+    res.sendFile(__dirname+"/ownerLogin.html")
+});
+
+// **********Get Request For Owner Registration**************//
+
+app.get("/ownerRegister",function(req,res){
+    res.sendFile(__dirname+"/ownerReg.html");
+});
+
+
+
+
+
+// **********Post Request For Customer Registration**************//
 
 app.post("/register",function(req,res){
 
@@ -57,6 +121,9 @@ newUser.save(function (err) {
 
 })
 
+
+// **********Post Request For Customer Login**************//
+
 app.post("/login",function(req,res){
 
     console.log(req.body);
@@ -76,11 +143,54 @@ app.post("/login",function(req,res){
 })
 
 
+// **********Post Request For Owner Login**************//
+
+app.post("/ownerlogin",function(req,res){
+    
+    console.log(req.body);
+    res.send(`<h2>Welcom to Admin Panel Mr. <h1> ${req.body.ownerId}</h1></h2>`)
+    
+
+})
+
+
+
+// **********Post Request For Owner Registarion**************//
+
+app.post("/ownerRegistration",function(req,res){
+
+    console.log(req.body);
+
+    console.log("id="+req.body.ownerId)
+    console.log("name= "+req.body.ownerName)
+    console.log("password="+req.body.password)
+
+    const newOwner=new Owner({
+        name:req.body.ownerName,
+        email:req.body.ownerId,
+        password:req.body.password
+    });
+
+    newOwner.save(function(err){
+        if(!err){
+            res.send(`<h2>You are Succesfully registered to Admin Panel <br/> Mr. <h1> ${req.body.ownerName}</h1></h2>`);
+
+        }else{
+            console.log(err);
+        }
+            
+        
+    });
+
+});
 
 
 
 
 
+
+
+// **********Listeninig Port**************//
 
 app.listen(3000,function(req,res){
     console.log("Server is running on localhost 3000");
